e166de35388e1595d94cd0f520d5f10d86b4a783
laboratorio <laboratorio@UPOAULA10217>
2024-10-28 07:37:09 -0500

    PruebaCambio

---
+++ ExamenProgreso1/Models/Celular.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamenProgreso1.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Modelo { get; set; }
+
+        public int Año { get; set; }
+
+        public float Precio { get; set; }
+    }
+}
+id, modelo, año, precio
---
+++ ExamenProgreso1/Models/JVillalba.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamenProgreso1.Models
+{
+    public class JVillalba
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public decimal Decimal { get; set; }
+
+        [Range(4, 10)] 
+        [Required]
+        public string Palabra { get; set; }
+
+        [Required]
+        public bool Respuesta { get; set; }
+
+        [Required]
+        public DateTime Fecha { get; set; }
+    }
+}
